7624b639056c7051d2f00033ed8860c802e8f4ca
redis1 <yunhaom94@outlook.com>
2018-07-13 17:54:53 -0400

    updated create-cluster script and some test results

--- Extras/create-cluster-mod/create-cluster-mod.py
+++ Extras/create-cluster-mod/create-cluster-mod.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import argparse
 import subprocess
+import os
 
 
 '''
@@ -9,6 +10,7 @@ Will run n nodes, set them slots and assgin them to each other's slave(to set th
 '''
 
 if __name__ == '__main__':
+    FNULL = open(os.devnull, 'w')
     parser = argparse.ArgumentParser()
     parser.add_argument("num_nodes", type=int, help="number of nodes")
     num_nodes = parser.parse_args().num_nodes
@@ -24,17 +26,18 @@ if __name__ == '__main__':
 --cluster-enabled yes --cluster-config-file nodes-{port}.conf \
 --cluster-node-timeout 2000 --appendonly yes --appendfilename \
 appendonly-{port}.aof --dbfilename dump-{port}.rdb --logfile {port}.log \
---daemonize yes".format(port=port)
-        subprocess.call(cmd, shell=True)
+--daemonize no --protected-mode no".format(port=port)
+        subprocess.Popen(cmd, shell=True, stdout=FNULL)
         #print(cmd)
 
+    print("Please wait for the cluster to be set up...")
 
     # meeting
     for i in range(1, num_nodes):
         port = port_base + i
 
         cmd = "../../src/redis-cli -p {port_base} CLUSTER MEET 127.0.0.1 {port}".format(port_base=port_base, port=port)
-        subprocess.call(cmd, shell=True)
+        subprocess.call(cmd, shell=True, stdout=FNULL)
 
 
     # setting slots:
@@ -42,7 +45,7 @@ appendonly-{port}.aof --dbfilename dump-{port}.rdb --logfile {port}.log \
         port = port_base + i
         for s in range(0, 16384):
             cmd = "../../src/redis-cli -p {port} CLUSTER ADDSLOTS {s}".format(port=port, s=s)
-            subprocess.call(cmd, shell=True)
+            subprocess.call(cmd, shell=True, stdout=FNULL)
 
     # getting cluster data
     cluster_nodes = {}
@@ -68,7 +71,7 @@ appendonly-{port}.aof --dbfilename dump-{port}.rdb --logfile {port}.log \
         for p, n in cluster_nodes.items():
             if str(p) != str(port):
                 cmd = "../../src/redis-cli -p {port} CLUSTER REPLICATE {node}".format(port=port, node=n)
-                subprocess.call(cmd, shell=True)
+                subprocess.call(cmd, shell=True, stdout=FNULL)
 
     try:
         print("Servers are running, use ctrl+c to stop and clean the cluster")
@@ -85,9 +88,9 @@ appendonly-{port}.aof --dbfilename dump-{port}.rdb --logfile {port}.log \
         print("stopping and cleaning: " + str(port))
         
         cmd = "../../src/redis-cli -p {port} shutdown nosave".format(port=port)
-        subprocess.call(cmd, shell=True)
+        subprocess.call(cmd, shell=True, stdout=FNULL)
 
 
     # cleaning
     cmd = "rm -rf *.log appendonly*.aof dump*.rdb nodes*.conf"
-    subprocess.call(cmd, shell=True)
+    subprocess.call(cmd, shell=True, stdout=FNULL)
